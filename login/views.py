50896ad46bd4efc7851c3b38ddc3c7c874a973d7
jungjaechoi <joong8812@gmail.com>
2022-01-27 09:55:01 +0900

    add - login url 및 view 추가

--- login/views.py
+++ login/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        return render(request, 'login/login.html')
