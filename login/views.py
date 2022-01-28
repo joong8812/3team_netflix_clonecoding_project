5b1bf452f515aedf1265715ec91708d4568f7cbb
jungjaechoi <joong8812@gmail.com>
2022-01-28 18:59:54 +0900

    add - login 백엔드 추가, gitignore 내용 추가

--- login/views.py
+++ login/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
 
 # Create your views here.
 def login_view(request):
     if request.method == 'POST':
-        pass
+        email = request.POST.get('email', None)
+        password = request.POST.get('password', None)
+        
+        me = auth.authenticate(request, email=email, password=password)
+
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/main')  # 로그인 후 유저네임을 응답한다
+        else:
+            return render(request, 'login/login.html', {'error': '유저이름 혹은 패스워드를 확인 해 주세요'})
+
     elif request.method == 'GET':
-        return render(request, 'login/login.html')
+            return render(request, 'login/login.html')
