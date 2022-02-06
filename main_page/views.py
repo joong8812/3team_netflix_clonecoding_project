9b61b34813ebef1b13931d14cf72ef98f2a62cdb
joneheart <tladlf33@naver.com>
2022-02-06 14:35:00 +0900

    수정 - db에서 특정이미지 불러오기

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import MovieModel
+
 
 # Create your views here.
+def recommend_movies(request):
+    if request.method == 'GET':
+        code = list(MovieModel.objects.filter(genre='코미디|액션').values('movie_id'))
+        genre_code = []
+        for i in range(12):
+            genre_code.append(code[i]['movie_id'])
+
+        print(genre_code)
+
+        # print(list(MovieModel.objects.all().values()))
+        return render(request, 'main_page.html', {'genre_code': genre_code})
+
