f004417b5c9a9933dd337a3dc78ff658444573c2
joneheart <tladlf33@naver.com>
2022-02-06 18:17:53 +0900

    추가 - s3에서 이미지 받아오도록 수정

--- main_page/views.py
+++ main_page/views.py
@@ -6,11 +6,14 @@ from .models import MovieModel
 def recommend_movies(request):
     if request.method == 'GET':
         code = list(MovieModel.objects.filter(genre='코미디|액션').values('movie_id'))
+        cast = list(MovieModel.objects.filter(cast='황정민|한혜진').values('movie_id'))
         genre_code = []
+        cast_code = []
         for i in range(12):
             genre_code.append(code[i]['movie_id'])
+            # cast_code.append(cast[i]['movie_id'])
 
-        print(genre_code)
+        print(cast)
 
         # print(list(MovieModel.objects.all().values()))
         return render(request, 'main_page.html', {'genre_code': genre_code})
