b024828a18e38e179a9c5473f724d4b95c348552
joneheart <tladlf33@naver.com>
2022-02-07 21:58:56 +0900

    추천 모델 연습

--- main_page/views.py
+++ main_page/views.py
@@ -6,14 +6,18 @@ from .recommender.movie_recommender import MovieRecommender
 def recommend_movies(request):
     if request.method == 'GET':
         mr = MovieRecommender()
-        code = mr.get_similar_movies_on_ibcf('161967', 12)
-        genre_code = []
-        movie_list = MovieRecommender().get_similar_movies_on_lfcf('161967')
+        similar = mr.get_similar_movies_on_ibcf('161967', 12)
+        similar_recommend = MovieRecommender().get_similar_movies_on_lfcf('161967', 12)
+        similar_genre_code = MovieRecommender().get_movies_on_same_genre('93728', 12)
+        similar_code = []
+        similar_recommend_code = []
+        for i in similar:
+            similar_code.append(str(i))
+        for i in range(12):
+            similar_recommend_code.append(str(similar_recommend[i][1]))
 
-        for i in code:
-            genre_code.append(str(i))
+        print(similar_genre_code)
 
-        print(movie_list)
-
-        return render(request, 'main_page/main_page.html', {'genre_code': genre_code, 'movie_list':movie_list})
+        return render(request, 'main_page/main_page.html', {'similar_code': similar_code, 'similar_recommend_code':similar_recommend_code,
+                                                            'similar_genre_code':similar_genre_code})
 
