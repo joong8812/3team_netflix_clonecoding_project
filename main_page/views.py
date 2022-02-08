316138e9409bb64336f0b23867ac1795e3e48f82
joneheart <tladlf33@naver.com>
2022-02-08 23:12:16 +0900

    수정 - 모달창 수정

--- main_page/views.py
+++ main_page/views.py
@@ -14,12 +14,18 @@ import random
 def recommend_movies(request):
     if request.method == 'GET':
         mr = MovieRecommender()
+
         similar = mr.get_similar_movies_on_ibcf('161967', 12)
         similar_recommend = MovieRecommender().get_similar_movies_on_lfcf('161967', 12)
         similar_genre = MovieRecommender().get_movies_on_same_genre('93728', 12)
+        same_director = MovieRecommender().get_movies_on_same_director('93728')
+        on_release = MovieRecommender().get_movies_on_release(end_month=2, amounts=12)
         similar_recommend_code = []
+        similar_recommend_code_2 = []
         movie_list = []
         similar_genre_code = []
+        same_director_list = []
+        on_release_list = []
         for i in similar:
             movie_info = {}
             movie = MovieModel.objects.get(movie_id=i)
@@ -43,12 +49,50 @@ def recommend_movies(request):
             movie_ginfo['cast'] = movie.cast
             movie_ginfo['plot'] = movie.plot
             similar_genre_code.append(movie_ginfo)
+
         for i in range(12):
             similar_recommend_code.append(str(similar_recommend[i][1]))
+        for f in similar_recommend_code:
+            similar_recommend_dict= {}
+            movie = MovieModel.objects.get(movie_id=f)
+            similar_recommend_dict['movie_id'] = movie.movie_id
+            similar_recommend_dict['title'] = movie.title
+            similar_recommend_dict['genre'] = movie.genre
+            similar_recommend_dict['maturity_rating'] = movie.maturity_rating
+            similar_recommend_dict['director'] = movie.director
+            similar_recommend_dict['cast'] = movie.cast
+            similar_recommend_dict['plot'] = movie.plot
+            similar_recommend_code_2.append(similar_recommend_dict)
+
+        for p in same_director:
+            same_director_dict= {}
+            movie = MovieModel.objects.get(movie_id=p)
+            same_director_dict['movie_id'] = movie.movie_id
+            same_director_dict['title'] = movie.title
+            same_director_dict['genre'] = movie.genre
+            same_director_dict['maturity_rating'] = movie.maturity_rating
+            same_director_dict['director'] = movie.director
+            same_director_dict['cast'] = movie.cast
+            same_director_dict['plot'] = movie.plot
+            same_director_list.append(same_director_dict)
+
+        for v in on_release:
+            on_release_dict= {}
+            movie = MovieModel.objects.get(movie_id=v)
+            on_release_dict['movie_id'] = movie.movie_id
+            on_release_dict['title'] = movie.title
+            on_release_dict['genre'] = movie.genre
+            on_release_dict['maturity_rating'] = movie.maturity_rating
+            on_release_dict['director'] = movie.director
+            on_release_dict['cast'] = movie.cast
+            on_release_dict['plot'] = movie.plot
+            on_release_list.append(on_release_dict)
+
 
         main_movie_clip = get_main_movie_clip()
-        return render(request, 'main_page/main_page.html', {'movie_list': movie_list, 'similar_recommend_code': similar_recommend_code,
-                                                            'similar_genre_code': similar_genre_code, 'main_movie_clip': main_movie_clip})
+        return render(request, 'main_page/main_page.html', {'movie_list': movie_list, 'similar_recommend_code': similar_recommend_code_2,
+                                                            'similar_genre_code': similar_genre_code, 'main_movie_clip': main_movie_clip,
+                                                            'same_director_list':same_director_list, 'on_release_list':on_release_list})
 
 
 # 영화 클립 url을 돌려준다
